60364a4c43261f2a1482d3ba3c30a2cea8bf3ea6
Novian Deny <anggerdeni@gmail.com>
2022-12-29 14:24:21 +0700

    feat: refactor nvim conf

---
+++ config/nvim/lua/an0nvi/init.lua
@@ -0,0 +1,31 @@
+require("an0nvi/remap")
+local is_bootstrap = require("an0nvi/packer")
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+require("an0nvi/setting")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
