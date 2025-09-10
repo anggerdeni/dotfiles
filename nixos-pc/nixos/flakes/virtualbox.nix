9dc2c62bf31c62337677cb54aae9bf6f17e1dfcf
Novian Deny <anggerdeni@gmail.com>
2025-09-11 06:37:35 +0700

    chore: trying to get vbox to work

--- nixos-pc/nixos/flakes/virtualbox.nix
+++ nixos-pc/nixos/flakes/virtualbox.nix
@@ -2,7 +2,10 @@
 
 {
   virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
   users.users.nov.extraGroups = [ "vboxusers" ];
+  virtualisation.virtualbox.host.enableHardening = false;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 }
 
